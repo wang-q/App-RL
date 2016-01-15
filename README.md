688847b029a57bbb3d6370f1d619477d464b1202
wang-q <wang-q@outlook.com>
2016-01-15 17:14:28 +0800

    Changes file

--- README.md
+++ README.md
@@ -6,23 +6,26 @@ App::RL - operating chromosome runlist files
 
     runlist <command> [-?h] [long options...]
         -? -h --help    show help
-    
+
     Available commands:
-    
+
       commands: list the application's commands
           help: display a command's help screen
-    
+
+       compare: compare 2 chromosome runlists
          merge: merge runlist yaml files
+         split: split runlist yaml files
           stat: coverage on chromosomes for runlists
 
 See `runlist commands` for usage information.
 
-# LICENSE
-
-Copyright 2015- Qiang Wang
-
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-
 # AUTHOR
 
-Qiang Wang
+Qiang Wang &lt;wang-q@outlook.com>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Qiang Wang.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/App/RL.pm
+++ lib/App/RL.pm
@@ -1,6 +1,6 @@
 package App::RL;
 
-our $VERSION = '0.1.3';
+our $VERSION = '0.1.4';
 
 use App::Cmd::Setup -app;
 
@@ -15,28 +15,29 @@ App::RL - operating chromosome runlist files
 =head1 SYNOPSIS
 
     runlist <command> [-?h] [long options...]
-        -? -h --help    show help
-    
+    	-? -h --help    show help
+
     Available commands:
-    
+
       commands: list the application's commands
           help: display a command's help screen
-    
+
+       compare: compare 2 chromosome runlists
          merge: merge runlist yaml files
+         split: split runlist yaml files
           stat: coverage on chromosomes for runlists
 
 See C<runlist commands> for usage information.
 
-=cut
-
-=head1 LICENSE
-
-Copyright 2015- Qiang Wang
-
-This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-
 =head1 AUTHOR
 
-Qiang Wang
+Qiang Wang <wang-q@outlook.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Qiang Wang.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
