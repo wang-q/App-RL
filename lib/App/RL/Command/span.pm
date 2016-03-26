d1df227d51d546d01f3db57589015af3449b5c27
wang-q <wang-q@outlook.com>
2016-03-27 05:48:06 +0800

    runlist span

---
+++ lib/App/RL/Command/span.pm
@@ -0,0 +1,110 @@
+package App::RL::Command::span;
+
+use App::RL -command;
+use App::RL::Common qw(:all);
+
+use constant abstract => 'operating spans in runlists';
+
+sub opt_spec {
+    return (
+        [ "outfile|o=s", "Output filename. [stdout] for screen." ],
+        [   "op=s",
+            "operations: cover, holes, trim, pad, excise or fill. Default is [cover]",
+            { default => "cover" }
+        ],
+        [   "number|n=i",
+            "Apply this number to trim, pad, excise or fill. Default is [0]",
+            { default => 0 }
+        ],
+        [ "remove|r", "Remove 'chr0' from chromosome names." ],
+    );
+}
+
+sub usage_desc {
+    my $self = shift;
+    my $desc = $self->SUPER::usage_desc;    # "%c COMMAND %o"
+    $desc .= " <infile>";
+    return $desc;
+}
+
+sub description {
+    my $desc;
+    $desc .= "List of operations.\n";
+    $desc .= " " x 4 . "cover:  a single span from min to max;\n";
+    $desc .= " " x 4 . "holes:  all the holes in runlist;\n";
+    $desc .= " " x 4 . "trim:   remove N integers from each end of each span of runlist;\n";
+    $desc .= " " x 4 . "pad:    add N integers from each end of each span of runlist;\n";
+    $desc .= " " x 4 . "excise: remove all spans smaller than N;\n";
+    $desc .= " " x 4 . "fill:   fill in all holes smaller than N.\n";
+    return $desc;
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+
+    $self->usage_error("This command need one input files.") unless @$args;
+    $self->usage_error("The input file [@{[$args->[0]]}] doesn't exist.")
+        unless -e $args->[0];
+
+    if ( $opt->{op} =~ /^cover/i ) {
+        $opt->{op} = 'cover';
+    }
+    elsif ( $opt->{op} =~ /^hole/i ) {
+        $opt->{op} = 'holes';
+    }
+    elsif ( $opt->{op} =~ /^trim/i ) {
+        $opt->{op} = 'trim';
+    }
+    elsif ( $opt->{op} =~ /^pad/i ) {
+        $opt->{op} = 'pad';
+    }
+    elsif ( $opt->{op} =~ /^excise/i ) {
+        $opt->{op} = 'excise';
+    }
+    elsif ( $opt->{op} =~ /^fill/i ) {
+        $opt->{op} = 'fill';
+    }
+    else {
+        Carp::confess "[@{[$opt->{op}]}] invalid\n";
+    }
+
+    if ( !exists $opt->{outfile} ) {
+        $opt->{outfile} = $opt->{op} . ".yml";
+    }
+}
+
+sub execute {
+    my ( $self, $opt, $args ) = @_;
+
+    #----------------------------#
+    # Loading
+    #----------------------------#
+    my $s_of = runlist2set( YAML::Syck::LoadFile( $args->[0] ), $opt->{remove} );
+
+    #----------------------------#
+    # Operating
+    #----------------------------#
+    my $op_result_of = {};
+    for my $chr ( keys %{$s_of} ) {
+        my $op     = $opt->{op};
+        my $op_set = $s_of->{$chr}->$op( $opt->{number} );
+        $op_result_of->{$chr} = $op_set->runlist;
+    }
+
+    #----------------------------#
+    # Output
+    #----------------------------#
+    my $out_fh;
+    if ( lc( $opt->{outfile} ) eq "stdout" ) {
+        $out_fh = *STDOUT;
+    }
+    else {
+        open $out_fh, ">", $opt->{outfile};
+    }
+    print {$out_fh} YAML::Syck::Dump($op_result_of);
+
+    close $out_fh;
+    return;
+}
+
+1;
---
+++ t/08-span.t
@@ -0,0 +1,14 @@
+use Test::More tests => 4;
+use App::Cmd::Tester;
+
+use App::RL;
+
+my $result = test_app( 'App::RL' => [qw(span t/brca2.yml --op cover -o stdout)] );
+
+is( ( scalar grep {/\S/} split( /\n/, $result->stdout ) ), 2, 'line count' );
+like( $result->stdout, qr{32316461\-32398770}, 'cover' );
+
+$result = test_app( 'App::RL' => [qw(span t/brca2.yml --op fill -n 1000 -o stdout)] );
+
+is( ( scalar grep {/\S/} split( /\n/, $result->stdout ) ), 2, 'line count' );
+like( $result->stdout, qr{32325076\-32326613}, 'fill' );
