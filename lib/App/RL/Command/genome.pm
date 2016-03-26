c74e14f9b440fe9fe129b00d20c06a1d5e13053f
wang-q <wang-q@outlook.com>
2016-03-27 04:30:18 +0800

    runlist genome

---
+++ lib/App/RL/Command/genome.pm
@@ -0,0 +1,68 @@
+package App::RL::Command::genome;
+
+use App::RL -command;
+use App::RL::Common qw(:all);
+
+use constant abstract => 'convert chr.size to runlists';
+
+sub opt_spec {
+    return (
+        [ "outfile|o=s", "Output filename. [stdout] for screen." ],
+        [ "remove|r",    "Remove 'chr0' from chromosome names." ],
+    );
+}
+
+sub usage_desc {
+    my $self = shift;
+    my $desc = $self->SUPER::usage_desc;    # "%c COMMAND %o"
+    $desc .= " <infile>";
+    return $desc;
+}
+
+sub description {
+    my $desc;
+    $desc .= "Full genome runlists.\n";
+    return $desc;
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+
+    $self->usage_error("This command need one input file.") unless @$args;
+    $self->usage_error("The input file [@{[$args->[0]]}] doesn't exist.")
+        unless -e $args->[0];
+
+    if ( !exists $opt->{outfile} ) {
+        $opt->{outfile} = Path::Tiny::path( $args->[0] )->absolute . ".yml";
+    }
+}
+
+sub execute {
+    my ( $self, $opt, $args ) = @_;
+
+    #----------------------------#
+    # Loading
+    #----------------------------#
+    my $length_of = read_sizes( $args->[0], $opt->{remove} );
+
+    my $r_of = {};
+    for my $key ( keys %{$length_of} ) {
+        my $set = AlignDB::IntSpan->new;
+        $set->add_pair( 1, $length_of->{$key} );
+        $r_of->{$key} = $set->runlist;
+    }
+
+    my $out_fh;
+    if ( lc( $opt->{outfile} ) eq "stdout" ) {
+        $out_fh = *STDOUT;
+    }
+    else {
+        open $out_fh, ">", $opt->{outfile};
+    }
+    print {$out_fh} YAML::Syck::Dump($r_of);
+
+    close $out_fh;
+    return;
+}
+
+1;
---
+++ t/07-genome.t
@@ -0,0 +1,10 @@
+use Test::More tests => 2;
+use App::Cmd::Tester;
+
+use App::RL;
+
+my $result = test_app(
+    'App::RL' => [qw(genome t/chr.sizes -o stdout)] );
+
+is( ( scalar grep {/\S/} split( /\n/, $result->stdout ) ), 17, 'line count' );
+like( $result->stdout, qr{I\:\s+1\-230218}, 'first chromosome' );
